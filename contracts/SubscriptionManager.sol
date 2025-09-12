638c68099aa6c8e1ce845542aaae4a76fdc2a849
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-09-13 01:16:31 +0530

    Update SubscriptionManager.sol

--- contracts/SubscriptionManager.sol
+++ contracts/SubscriptionManager.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./SmartWallet.sol";
-
+ 
 /**
  * @title SubscriptionManager
  * @dev Manages gasless subscription plans and billing using ERC-4337 account abstraction
