69924a82770f75ec7a1ef248d20cf283caf571a0
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-09-26 10:57:43 +0530

    Update IntegrationRegistry.sol

--- contracts/IntegrationRegistry.sol
+++ contracts/IntegrationRegistry.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";    
+import "@openzeppelin/contracts/security/Pausable.sol";     
 
 /** 
  * @title IntegrationRegistry 
