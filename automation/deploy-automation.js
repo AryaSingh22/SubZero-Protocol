aedaafc2b644b2b57dfdb7eb001c63efa981da09
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-10-06 23:28:14 +0530

    Update deploy-automation.js

--- automation/deploy-automation.js
+++ automation/deploy-automation.js
@@ -15,7 +15,7 @@ async function main() {
       137: "0x3CACa7b48D0573D611c5f7C582447468a4d04671", // Polygon Mainnet
       80001: "0xF82D64357D9120a760e1E4C75f646E0618e5d895", // Mumbai Testnet
       31337: deployer.address // Use deployer for local testing
-    },
+    }, 
     // Subscription Manager (should be already deployed)
     subscriptionManager: process.env.SUBSCRIPTION_MANAGER_ADDRESS || "",
   };
