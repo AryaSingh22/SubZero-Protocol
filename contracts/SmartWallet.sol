4a7621e4809be35d84312276e889e9327746dcd0
AryaSingh22 <138510479+AryaSingh22@users.noreply.github.com>
2025-11-22 23:54:49 +0530

    Update SmartWallet.sol

--- contracts/SmartWallet.sol
+++ contracts/SmartWallet.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol"; 
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 interface IEntryPoint {
